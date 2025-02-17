70bd7224299bbab51f291d932d9145ad2cb9dd3d
phj3137 <phj3137@naver.com>
2025-02-17 16:07:01 +0900

    +layout

--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,3 +1,7 @@
+import styles from "../../styles/about-us.module.css";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}&copy; Next JS is great!</div>;
+  return (
+    <div className={styles.layout}>{children}&copy; Next JS is great!</div>
+  );
 }
